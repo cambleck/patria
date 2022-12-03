d26aa1b7d6a7cd473f7e6cac17eb21d80696babd
cambleck <bleckcam@gmail.com>
2022-12-02 23:01:45 -0600

    psql

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,31 @@
 import "../styles/globals.css";
+import { User } from "../typings";
+import Header from "./Header";
+
+let date: Date = new Date();
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user: User = {
+    id: "asdfasdf",
+    name: "asdfasdf",
+    password: "asdfasdf",
+    email: "asdfasdf",
+    date: date,
+  };
+  console.log(user);
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <Header {...user} />
+        <div className="flex justify-center items-center w-full">
+          <div className="w-full max-w-3xl">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,41 @@
+import Link from "next/link";
 import React from "react";
+import { Pool } from "pg";
 
-export default function HomePage() {
-  return <div>HomePage</div>;
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  database: "instagram",
+  password: "",
+});
+
+async function getPosts() {
+  const client = await pool.connect();
+  const result = await client.query("SELECT * FROM posts");
+  console.log(result);
+  client.release();
+  return result.rows;
+}
+
+export default async function HomePage() {
+  const posts = await getPosts();
+
+  console.log(posts);
+  return (
+    <div>
+      <article className="prose">
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked
+          to a series of rabies cases springing up around the country.
+        </p>
+      </article>
+    </div>
+  );
 }
